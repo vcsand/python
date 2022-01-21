fc8a6819c28cf2d478d220dd16cb3e84b9886abf
Voros Csanad <voroscsanad02@gmail.com>
2022-01-21 11:23:00 +0000

    fingerprint

---
+++ ujjlenyomat1.py
@@ -0,0 +1,17 @@
+import hashlib, bcrypt
+
+jelszo = input("Jelszo: ")
+#sha = hashlib.sha512(jelszo.encode("utf8")).hexdigest()
+#print(sha)
+so = bcrypt.gensalt()
+bcr = bcrypt.hashpw(jelszo.encode("utf8"), so)
+print(bcr)
+reply = bcrypt.checkpw(jelszo.encode("utf8"), bcr)
+
+jelszo2 = input("Add meg megegyszer: ")
+reply2 = bcrypt.checkpw(jelszo2.encode("utf8"), bcr)
+
+if reply2:
+	print("Szep munka katona!")
+else:
+	print("Te gyoker!")
