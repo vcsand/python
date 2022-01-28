5c4ec0ba8b0fde47e316d6c0dffea68ad1354928
Voros Csanad <voroscsanad02@gmail.com>
2022-01-28 11:54:24 +0000

    menethahaxd

--- menet1.py
+++ menet1.py
@@ -1,8 +1,8 @@
 import requests
 mm = requests.Session()
 try:
-	mm.get('https://httpbin.org/cookies/set/sessioncookie/123456789')
-	reply = mm.get('https://httpbin.org/cookies')
+	mm.get('http://httpbin.org/cookies/set/sessioncookie/123456789')
+	reply = mm.get('http://httpbin.org/cookies')
 except:
 	print("Hiba.")
 	exit()
