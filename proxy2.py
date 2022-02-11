5edde86b6a88b3565581fe1373a6bc39c8c76ea5
Voros Csanad <voroscsanad02@gmail.com>
2022-02-11 13:26:49 +0000

    pat

---
+++ proxy2.py
@@ -0,0 +1,10 @@
+import requests
+url = "https://postman-echo.com/basic-auth"
+#proxy = {'https:': "https://localhost:8080"}
+try:
+	reply = requests.get(url, auth=("postman", "password"))
+except:
+	print("Hiba!")
+	exit()
+print(reply.status_code)
+print(reply.text)
