e19679870d4f6d1fe6245146eec028ee1b7e56fa
vcsand <89984691+vcsand@users.noreply.github.com>
2022-06-10 11:27:53 +0200

    Update photobackup2.0.py

--- photobackup2.0.py
+++ photobackup2.0.py
@@ -3,7 +3,7 @@ import hashlib
 import os
 photo = input("Adj meg egy fajlnevet: ")
 url = "http://docker.lan:8420/"
-pw = input("Adj meg egy jelszot ocsinasz: ")
+pw = input("Adj meg egy jelszot: ")
 sha512 = hashlib.sha512(pw.encode("utf8")).hexdigest()
 fsize = os.path.getsize(photo)
 d={"password":sha512, "filesize": fsize}
@@ -12,6 +12,6 @@ try:
 	reply = requests.post(url, data = d, files = {"upfile": f})
 	f.close()
 except:
-	print("Szar van a palacsintaban.")
+	print("Hiba!")
 	exit()
 print(reply)
