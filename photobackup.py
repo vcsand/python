c9087d34f511044e1a04927330cd464f6d0af50c
vcsand <89984691+vcsand@users.noreply.github.com>
2022-06-10 11:27:17 +0200

    Update photobackup.py

--- photobackup.py
+++ photobackup.py
@@ -1,12 +1,12 @@
 import requests
 import hashlib
 url = "http://docker.lan:8420/test"
-pw = input("Adj meg egy jelszot ocsinasz: ")
+pw = input("Adj meg egy jelszot: ")
 sha512 = hashlib.sha512(pw.encode("utf-8")).hexdigest()
 d={"password":sha512}
 try:
 	reply = requests.post(url, data = d)
 except:
-	print("Szar van a palacsintaban.")
+	print("Hiba!")
 	exit()
 print(reply)
